ba7c2ffdcad9edcc5f04f731e2393277b8077f71
Mathias Lauridsen <mathiaslauridsen@sygeplejebutikken.dk>
2024-10-09 14:29:52 +0200

    Fixed SPA error

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 		adapter: adapter({
 			pages: 'build', // Output-mappen til GitHub Pages
 			assets: 'build', // Samme som pages, så assets også bliver lagt i build
-			fallback: 'index.html' // Ingen fallback (f.eks. hvis du ikke bruger en SPA)
+			fallback: null // Ingen fallback (f.eks. hvis du ikke bruger en SPA)
 		}),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/vaerktoejskassen' : '', // REPO-NAME er navnet på dit GitHub repo
